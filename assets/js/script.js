5be4d6786c84e9549c7d5034c43cf9a511535adb
Sean Hayes <hayessea000@outlook.com>
2023-04-15 23:27:40 -0500

    fixed bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,11 +40,13 @@ let getWeather = function (){
             let todayWeatherBox = $("#todayWeather")
             todayWeatherBox.html("")
             let todayWeatherBoxHeader = $("<div>")
+            todayWeatherBoxHeader.attr("class", "row")
             let todayWeatherBoxCity = $("<h2>")
+            todayWeatherBoxCity.text(`${todaysWeather.name} (${today})`)
             let todayWeatherBoxImage = $("<img>")
+            todayWeatherBoxImage.attr("id", "bigguy")
             todayWeatherBoxImage.attr("src", `https://openweathermap.org/img/wn/${todaysWeather.weather[0].icon}.png`)
             todayWeatherBoxImage.attr("alt", `${todaysWeather.weather[0].description}`)
-            todayWeatherBoxCity.text(`${todaysWeather.name} (${today})`)
             let todayWeatherBoxtemp = $("<p>")
             todayWeatherBoxtemp.text(`Temp: ${todaysWeather.main.temp} °F`)
             let todayWeatherBoxWind = $("<p>")
@@ -75,8 +77,8 @@ let getWeather = function (){
                 forecastWeatherCard.attr("class", "forecast-card")
                 let forecastWeatherDate = $("<h4>")
                 forecastWeatherDate.attr("class", "text-light")
-                // forecastWeatherDate.val(forecastWeather.list[3].dt_text)
-                // console.log(forecastWeather.list[3])
+                let forecastDate= dayjs(forecastWeather.list[i].dt_txt).format("M/D/YYYY")
+                forecastWeatherDate.text(forecastDate)
                 let forecastWeatherImage =$("<img>")
                 forecastWeatherImage.attr("src", `https://openweathermap.org/img/wn/${forecastWeather.list[i].weather[0].icon}.png`)
                 forecastWeatherImage.attr("alt", `${forecastWeather.list[i].weather[0].description}`)
@@ -89,7 +91,6 @@ let getWeather = function (){
                 let forecastWeatherHumidity =$("<p>")
                 forecastWeatherHumidity.attr("class", "text-light")
                 forecastWeatherHumidity.text(`Humidity: ${forecastWeather.list[i].main.humidity} %`)
-                
                 forecastWeatherCard.append(forecastWeatherDate)
                 forecastWeatherCard.append(forecastWeatherImage)
                 forecastWeatherCard.append(forecastWeatherTemp)
@@ -114,13 +115,11 @@ let searchHistory= function(){
         historyCard.attr("data-lat",`${savedCities[i].lat}`)
         historyCard.attr("data-lon",`${savedCities[i].lon}`)
         var historyDiv= $("<div>")
-
         historyDiv.append(historyCard)
         cityHistory.append(historyDiv)
     }
     let historySearch= $(".historyBtn")
     historySearch.on("click", function(event){
-        let tester= event.target
         cityLat = event.target.getAttribute("data-lat")
         console.log(cityLat)
         cityLon = event.target.getAttribute("data-lon")
@@ -141,19 +140,7 @@ searchBtn.on("click", function(event){
         getlocation()
     }
 })   
-// searchBtn.on("click", function(event){
-//     event.preventDefault()
-//     cityLat = event.target.getattr("data-lat")
-//     console.log(cityLat)
-//     cityLon = event.target.getattr("data-lon")
-//     console.log(cityLon)
-//     if(cityInputVal== ""){
-//         alert("Please enter a value to search for.");
-//         return;
-//     }else{
-//         getWeather()
-//     }
-// })   
+
 savedCities = JSON.parse(localStorage.getItem("savedCities"));
 if(savedCities== null){
     savedCities=[];
